e1cf6372f8a32b7afe4377d6af6e906b6f2f3046
oscartulio <51069335+oscartulio@users.noreply.github.com>
2019-11-12 20:12:43 -0600

    avance6

--- app/src/main/java/com/example/publieventos/CalendarActivity.kt
+++ app/src/main/java/com/example/publieventos/CalendarActivity.kt
@@ -1,30 +1,41 @@
 package com.example.publieventos
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.TimePicker
 import kotlinx.android.synthetic.main.agregar_evento_activity.*
 import java.util.*
+
 
 class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
-    /*---------------------------------------------------------------------------------------*/
+
+
     val calendar= Calendar.getInstance()
     val year = calendar.get(Calendar.YEAR)
     val month= calendar.get(Calendar.MONTH)+1
     val day = calendar.get(Calendar.DAY_OF_MONTH)
+    val hora = calendar.get(Calendar.HOUR)
+    val minute=calendar.get(Calendar.MINUTE)
+
+
 
         val y= year
         val m=month
         val d=day
+        val hors=hora
+        val mins=minute
 
 
     editText5.setText(""+ d +"/"+ m +"/"+ y)
 
+        editTextTime.setText(""+hors +":"+mins)
 
     imageView2.setOnClickListener{
 
@@ -35,7 +46,12 @@ class CalendarActivity : AppCompatActivity() {
         dpd.show()}
 
 
-/*---------------------------------------------------------------------------------------*/
+        imageView3.setOnClickListener{
+            val dpd2 =
+                TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { view: TimePicker, hourOfDay, minute ->
+                    editTextTime.setText(""+hourOfDay +":"+minute)
+                },hora,minute)
+            dpd2.show() }
 
 
     }
