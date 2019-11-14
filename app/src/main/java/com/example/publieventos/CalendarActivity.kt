c6df36ef1b12b227873763b202141b98de4a8719
oscartulio <51069335+oscartulio@users.noreply.github.com>
2019-11-13 20:24:28 -0600

    avance77

--- app/src/main/java/com/example/publieventos/CalendarActivity.kt
+++ app/src/main/java/com/example/publieventos/CalendarActivity.kt
@@ -2,57 +2,92 @@ package com.example.publieventos
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
 import android.widget.TimePicker
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_calendar.*
 import kotlinx.android.synthetic.main.agregar_evento_activity.*
+import kotlinx.android.synthetic.main.agregar_evento_activity.editText5
+import kotlinx.android.synthetic.main.agregar_evento_activity.imageView2
+import java.text.SimpleDateFormat
 import java.util.*
 
 
-class CalendarActivity : AppCompatActivity() {
+class CalendarActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-
-    val calendar= Calendar.getInstance()
-    val year = calendar.get(Calendar.YEAR)
-    val month= calendar.get(Calendar.MONTH)+1
-    val day = calendar.get(Calendar.DAY_OF_MONTH)
-    val hora = calendar.get(Calendar.HOUR)
-    val minute=calendar.get(Calendar.MINUTE)
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
 
 
-
-        val y= year
-        val m=month
-        val d=day
-        val hors=hora
-        val mins=minute
+        val y = year
+        val m = month
+        val d = day
 
 
-    editText5.setText(""+ d +"/"+ m +"/"+ y)
-
-        editTextTime.setText(""+hors +":"+mins)
-
-    imageView2.setOnClickListener{
-
-        val dpd =
-            DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view: DatePicker, year: Int, month:Int, dayOfMonth:Int ->
-                editText5.setText(""+ dayOfMonth +"/"+month+"/"+year+"")
-            },year,month,day)
-        dpd.show()}
+        editText5.setText("" + d + "/" + m + "/" + y)
 
 
-        imageView3.setOnClickListener{
-            val dpd2 =
-                TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { view: TimePicker, hourOfDay, minute ->
-                    editTextTime.setText(""+hourOfDay +":"+minute)
-                },hora,minute)
-            dpd2.show() }
+
+
+        imageView2.setOnClickListener {
+
+            val dpd =
+                DatePickerDialog(
+                    this,
+                    DatePickerDialog.OnDateSetListener { view: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                        editText5.setText("" + dayOfMonth + "/" + month + "/" + year + "") },
+                    year, month, day)
+            dpd.show()
+        }
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
+        BtnSave.setOnClickListener{
+           // val event = txt1.text.toString().trim()
+            val fecha = editText5.toString().trim()
+            //val hora = editText.text.toString().trim()
+            //val  descripcion = editText4.text.toString().trim()
+
+            val editor = sharedPreferences.edit()
+
+            //editor.putString("Evento", event)
+            editor.putString("Fecha", fecha)
+           // editor.putString("Hora", hora)
+            //editor.putString("Descripcion", hora)
+            editor.apply()
+             Toast.makeText(this,"Guardado satisfactoriamente",Toast.LENGTH_LONG).show()
+
+        }
+
+        BtnSave.setOnClickListener {
+
+               // val event = sharedPreferences.getString("Evento", "")
+                val fecha = sharedPreferences.getString("Fecha", "")
+               // val hora = sharedPreferences.getString("Hora", "")
+               // val descripcion = sharedPreferences.getString("Descripcion", "")
+
+                textView3.text = "Fecha: $fecha  "
+
+
+        }
+
+
+
+
+       }
+
 
 
     }
-}
+
+
---
+++ app/src/main/java/com/example/publieventos/ShowInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.publieventos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_information)
+    }
+}
